1ada1d5371c739231de5dde75b4b5202a0a22343
fandipres <fandi.simamora@gmail.com>
2021-09-08 23:11:19 +0700

    add SC C menghitung keliling dan luas persegi panjang

---
+++ C/Menghitung Seputar Persegi Panjang/Menghitung Keliling dan Luas Persegi Panjang.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//source code by hobingoding.com
+
+int main(){
+    int panjang, lebar, tinggi, keliling, luas;
+
+    //Input Variabel
+    printf("Program Menghitung Keliling dan Luas Persegi Panjang\n");
+    printf("source code by hobingoding.com\n");
+    printf("\nInput Panjang\t: ");
+    scanf("%d", &panjang);
+    printf("Input Lebar  \t: ");
+    scanf("%d", &lebar);
+
+    //Menghitung Keliling dan Luas
+    keliling = (2 * panjang) + (2 * lebar);
+    luas = panjang * lebar;
+    printf("Keliling \t: %d\n", keliling);
+    printf("Luas \t\t: %d\n", luas);
+    return 0;
+}
