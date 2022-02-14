eb011c4f349e20df60d9bc86c6ad29babcc439e9
fandipres <fandi.simamora@gmail.com>
2022-02-14 11:00:16 +0700

    add SC C menghitung luas trapesium

---
+++ C/Menghitung Seputar Trapesium/Menghitung Luas Trapesium.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//visit us @hobingoding.com
+
+int main(){
+    int sisi1, sisi2, tinggi;
+    int luas;
+
+    //Input Variabel
+    printf("Program Menghitung Luas Trapesium\n\n");
+    printf("Input Sisi-1 : ");
+    scanf("%d", &sisi1);
+    printf("Input Sisi-2 : ");
+    scanf("%d", &sisi2);
+    printf("Input Tinggi : ");
+    scanf("%d", &tinggi);
+
+    //Menghitung Luas
+    tinggi = (sisi1 + sisi2) * tinggi / 2;
+    printf("Luas         : %d cm^2\n", tinggi);
+    return 0;
+}
