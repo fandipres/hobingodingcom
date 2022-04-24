0a9d8154294604d32bd527b7110990148ca0f040
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2022-04-24 11:14:10 +0700

    add SC Python menghitung keliling dan luas persegi

---
+++ Python/Menghitung Seputar Persegi/Menghitung Keliling dan Luas Persegi.py
@@ -0,0 +1,7 @@
+print("Program Menghitung Keliling dan Luas Persegi")
+#visit us @hobingoding.com
+print()
+
+sisi = int(input("Sisi : "))
+print("Keliling : ", 4 * sisi)
+print("Luas : ", sisi * sisi)
