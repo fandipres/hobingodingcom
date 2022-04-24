68a69920f12ac2da591d4c11566363f74e283304
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2022-04-24 10:37:09 +0700

    tambah komentar menghitung volume balok

--- C/Menghitung Seputar Balok/Menghitung Volume Balok.c
+++ C/Menghitung Seputar Balok/Menghitung Volume Balok.c
@@ -3,7 +3,8 @@
 
 int main(){
     int panjang, lebar, tinggi, volume;
-
+    
+    //Input Variabel
     printf("Program Menghitung Volume Balok\n");
     printf("source code by hobingoding.com\n");
     printf("\nInput Panjang\t: ");
@@ -13,9 +14,8 @@ int main(){
     printf("Input Tinggi \t: ");
     scanf("%d", &tinggi);
 
+    //Menghitung Volume
     volume = panjang * lebar * tinggi;
     printf("Volume Balok \t: %d cm^3\n", volume);
-
-    getch();
     return 0;
 }
