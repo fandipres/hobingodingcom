76dbb4b7255cac72730c45465cc5e5d7be72f2c8
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2022-04-24 11:16:02 +0700

    add SC Python menghitung keliling dan luas persegi panjang

---
+++ Python/Menghitung Seputar Persegi Panjang/Menghitung Keliling dan Luas Persegi Panjang.py
@@ -0,0 +1,8 @@
+print("Program Menghitung Keliling dan Luas Persegi Panjang")
+#visit us @hobingoding.com
+print()
+
+panjang = int(input("Panjang : "))
+lebar = int(input("Lebar : "))
+print("Keliling : ", (2 * panjang) + (2 * lebar))
+print("Luas : ", panjang * lebar)
