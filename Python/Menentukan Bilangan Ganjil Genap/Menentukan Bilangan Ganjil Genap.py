56c52df82b22752d8377826e1ed8ec96d0ae4044
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2022-05-20 14:34:13 +0700

    add SC Python menentukan bilangan ganjil genap

---
+++ Python/Menentukan Bilangan Ganjil Genap/Menentukan Bilangan Ganjil Genap.py
@@ -0,0 +1,8 @@
+print("Mengecek Angka Ganjil atau Genap")
+print("source code by hobingoding.com")
+print()
+angka = int(input("Input Angka : "))
+if(angka % 2 == 0):
+    print("Angka", angka, "merupakan angka Genap.")
+else:
+    print("Angka", angka, "merupakan angka Ganjil.")
