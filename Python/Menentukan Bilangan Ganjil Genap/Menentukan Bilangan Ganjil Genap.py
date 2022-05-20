2d53c7ee644cc771db36b826927c28a3c4f5195b
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2022-05-21 05:20:27 +0700

    memperbaharui gaya penulisan kode

--- Python/Menentukan Bilangan Ganjil Genap/Menentukan Bilangan Ganjil Genap.py
+++ Python/Menentukan Bilangan Ganjil Genap/Menentukan Bilangan Ganjil Genap.py
@@ -1,7 +1,9 @@
 print("Mengecek Angka Ganjil atau Genap")
-print("source code by hobingoding.com")
+print("visit us @hobingoding.com")
 print()
+#Input Angka
 angka = int(input("Input Angka : "))
+#Pengecekan Angka
 if(angka % 2 == 0):
     print("Angka", angka, "merupakan angka Genap.")
 else:
