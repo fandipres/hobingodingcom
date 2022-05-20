6361fc224536f36d3bd8bcfbd92ff1a5446f3a3a
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2022-05-21 05:34:36 +0700

    memperbaharui gaya penulisan kode

--- Python/Menghitung Seputar Persegi Panjang/Menghitung Keliling dan Luas Persegi Panjang.py
+++ Python/Menghitung Seputar Persegi Panjang/Menghitung Keliling dan Luas Persegi Panjang.py
@@ -1,8 +1,9 @@
 print("Program Menghitung Keliling dan Luas Persegi Panjang")
-#visit us @hobingoding.com
+print("visit us @hobingoding.com")
 print()
-
-panjang = int(input("Panjang : "))
-lebar = int(input("Lebar : "))
+#Input Panjang dan Lebar
+panjang = int(input("Input Panjang : "))
+lebar = int(input("Input Lebar : "))
+#Hitung Keliling dan Luas
 print("Keliling : ", (2 * panjang) + (2 * lebar))
 print("Luas : ", panjang * lebar)
