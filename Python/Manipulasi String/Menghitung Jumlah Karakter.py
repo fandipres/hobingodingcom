5db27de904cfd8aeac884285f817577358cc50e0
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2022-12-12 17:17:40 +0700

    add SC Python menghitung jumlah karakter

---
+++ Python/Manipulasi String/Menghitung Jumlah Karakter.py
@@ -0,0 +1,7 @@
+print("Program Menghitung Jumlah Karakter")
+print("visit us @hobingoding.com")
+print()
+#Input Kalimat
+kalimat = input("Input String : ")
+#Hitung Jumlah Karakter
+print("Jumlah Karakter : ", len(kalimat))
