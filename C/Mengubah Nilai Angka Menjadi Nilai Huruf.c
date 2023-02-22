8e4b794add4c0c832ac177ba34f5c79f3340e635
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-02-22 18:07:41 +0700

    add SC C mengubah nilai angka menjadi nilai huruf

---
+++ C/Mengubah Nilai Angka Menjadi Nilai Huruf.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+//visit us @hobingoding.com
+
+int main(){
+    int nilai;
+
+    printf("Program Konversi Nilai Angka Menjadi Nilai Huruf\n\n");
+    printf("Input Nilai: ");
+    scanf("%d", &nilai);
+
+    if(nilai > 84 && nilai <= 100)
+        printf("Nilai Huruf: A");
+    else if(nilai > 79 && nilai <= 84)
+        printf("Nilai Huruf: A-");
+    else if(nilai > 74 && nilai <= 79)
+        printf("Nilai Huruf: B+");
+    else if(nilai > 69 && nilai <= 74)
+        printf("Nilai Huruf: B");
+    else if(nilai > 64 && nilai <= 69)
+        printf("Nilai Huruf: B-");
+    else if(nilai > 59 && nilai <= 64)
+        printf("Nilai Huruf: C+");
+    else if(nilai > 49 && nilai <= 59)
+        printf("Nilai Huruf: C");
+    else if(nilai > 39 && nilai <= 49)
+        printf("Nilai Huruf: D");
+    else if(nilai > 0 && nilai <= 39)
+        printf("Nilai Huruf: E");
+    else
+        printf("MAAF: Masukan Anda Salah");
+    printf("\n");
+
+    getch();
+    return 0;
+}
