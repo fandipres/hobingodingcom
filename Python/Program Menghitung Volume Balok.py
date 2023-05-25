ee001c68fed70f87025364428f23e397cf6a8f2d
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-05-25 19:47:30 +0700

    add SC Python menghitung volume balok

---
+++ Python/Program Menghitung Volume Balok.py
@@ -0,0 +1,11 @@
+print("Program Menghitung Volume Balok")
+print("visit us @hobingoding.com")
+print()
+#Input Panjang
+panjang = int(input("Input Panjang : "))
+#Input Lebar
+lebar = int(input("Input Lebar : "))
+#Input Tinggi
+tinggi = int(input("Input Tinggi : "))
+#Hitung Luas
+print("Volume : " panjang * lebar * tinggi)
