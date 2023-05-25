725395efce805d9359aecfcbb9ef5171d003a7f0
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-05-25 19:46:07 +0700

    add SC Python menghitung luas segitiga

---
+++ Python/Program Menghitung Luas Segitiga.py
@@ -0,0 +1,9 @@
+print("Program Menghitung Luas Segitiga")
+print("visit us @hobingoding.com")
+print()
+#Input Alas
+alas = int(input("Input Alas : "))
+#Input Tinggi
+tinggi = int(input("Input Tinggi : "))
+#Hitung Luas
+print("Luas : %.2f" % (alas * tinggi / 2))
