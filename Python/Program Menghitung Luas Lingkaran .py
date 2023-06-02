1897745cde004101eb6276a18d1b819698663437
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-06-02 17:53:14 +0700

    add SC Python menghitung luas lingkaran

---
+++ Python/Program Menghitung Luas Lingkaran .py
@@ -0,0 +1,7 @@
+print("Program Menghitung Luas Lingkaran")
+print("visit us @hobingoding.com")
+print()
+#Input Jari-jari
+r = int(input("Input Jari-jari : "))
+#Hitung Luas
+print("Luas : %.2f" % (3.14 * r * r))
