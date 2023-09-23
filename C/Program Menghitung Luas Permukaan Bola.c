ed9d7594ed778720f441301598d7a61209d185c8
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-24 02:35:33 +0700

    Update Program Menghitung Luas Permukaan Bola.c

--- C/Program Menghitung Luas Permukaan Bola.c
+++ C/Program Menghitung Luas Permukaan Bola.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-//source code by hobingoding.com
 
-int main(){
+int main() {
     int r;
-    float phi = 3.14, luasPermukaan;
+    float phi = 3.14, luas_permukaan;
 
-    //Input Variabel
     printf("Program Menghitung Luas Permukaan Bola\n");
-    printf("source code by hobingoding.com\n");
-    printf("\nInput Jari-jari : ");
+    printf("visit us hobingoding.com\n\n");
+    // Input Jari-jari
+    printf("Input Jari-jari : ");
     scanf("%d", &r);
 
-    //Menghitung Luas Permukaan
-    luasPermukaan = 4 * phi * r * r;
-    printf("Luas Permukaan  : %.2f\n", luasPermukaan);
+    // Hitung Luas Permukaan
+    luas_permukaan = 4 * phi * r * r;
+    printf("Luas Permukaan Bola : %.2f", luas_permukaan);
     return 0;
 }
