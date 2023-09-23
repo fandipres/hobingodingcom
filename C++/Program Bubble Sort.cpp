556b6646062868d5a3fb7634056d696ea5850e5f
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-23 17:22:54 +0700

    Update Program Bubble Sort.cpp

--- C++/Program Bubble Sort.cpp
+++ C++/Program Bubble Sort.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
-//visit us @hobingoding.com
 
 int main(){
-    int banyakData, i, j;
+    int banyak_data, i, j;
     
     cout << "Pengurutan Data dengan Algoritma Bubble Sort" << endl;
+    cout << "visit us hobingoding.com\n" << endl;
+    // Input Banyak Data
+    cout << "Input Banyak Data : ";
+    cin >> banyak_data;
+    int data[banyak_data];
     
-    cout << "Input Banyak Data       : ";
-    cin >> banyakData;
-    int data[banyakData];
-    
-    //Perulangan untuk menginput data sebanyak n
-    cout << "Data yang Belum Berurut : ";
-    for(i = 0; i < banyakData; i++)
+    // Input Data
+    cout << "Input Data : ";
+    for(i = 0; i < banyak_data; i++)
     {
         cin >> data[i];
     }
     
-    //Algoritma Bubble Sort
-    for(i = 0; i < banyakData; i++)
+    // Algoritma Bubble Sort
+    for(i = 0; i < banyak_data; i++)
     {
-        for(j = 0; j < banyakData-i-1; j++)
+        for(j = 0; j < banyak_data-i-1; j++)
         {
             if(data[j] > data[j+1])
             {
@@ -32,13 +32,12 @@ int main(){
         }
     }
     
-    //Pencetakan data yang sudah diurutkan
-    cout << "Hasil Pengurutan Data   : ";
-    for(i = 0; i < banyakData; i++)
+    // Hasil Pengurutan Data
+    cout << "Hasil Pengurutan Data : ";
+    for(i = 0; i < banyak_data; i++)
     {
         cout << data[i] << " ";
     }
-    
     cin.ignore();
     return 0;
 }
