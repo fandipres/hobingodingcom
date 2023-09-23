a1bc401b557f3297664b2193eebfc15587a32150
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-23 14:11:32 +0700

    Update Program Menentukan Bilangan Ganjil Genap.c

--- C/Program Menentukan Bilangan Ganjil Genap.c
+++ C/Program Menentukan Bilangan Ganjil Genap.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-//visit us @hobingoding.com
+// visit us @hobingoding.com
 
 int main(){
     int angka;
 
     printf("Program Mengecek Angka Ganjil atau Genap\n\n");
+    // Input Angka
     printf("Angka : ");
     scanf("%d", &angka);
 
+    // Pengecekan Angka
     if(angka % 2 ==0)
         printf("Angka %d merupakan angka Genap.", angka);
     else
