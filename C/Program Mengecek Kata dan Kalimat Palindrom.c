aaab730a6e47dc65d8a146dbfbfda0b6bdd9eb4d
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-23 21:22:36 +0700

    Update Program Mengecek Kata dan Kalimat Palindrom.c

--- C/Program Mengecek Kata dan Kalimat Palindrom.c
+++ C/Program Mengecek Kata dan Kalimat Palindrom.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-//visit us @hobingoding.com
 
-int main(){
+int main() {
     char kata[50] = {'\0'};
     char temp[50] = {'\0'};
     int n, i, j = 0;
 
-    printf("Program Mengecek Kata - Kalimat Palindrom atau Bukan\n\n");
+    printf("Program Mengecek Kata dan Kalimat Palindrom\n");
+    printf("visit us hobingoding.com\n\n");
+    // Input Kata
     printf("Input Kata : ");
     gets(kata);
     n = strlen(kata);
 
-    for(i = 0; kata[i]; i++){
+    // Ubah Kata Menjadi Lower Case
+    for(i = 0; kata[i]; i++) {
         kata[i] = tolower(kata[i]);
     }
 
-    for(i = n-1; i >= 0; i--){
+    // Balik Kata
+    for(i = n-1; i >= 0; i--) {
         temp[j] = kata[i];
         j++;
     }
 
-    printf("Result : ");
+    // Cek Palindrom
     if(strcmp(kata, temp) == 0)
-        printf("Palindrom");
+        printf("Hasil : Palindrom");
     else
-        printf("Bukan Palindrom");
-
-    getch();
+        printf("Hasil : Bukan Palindrom");
     return 0;
 }
