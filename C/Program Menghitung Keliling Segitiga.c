0d949999d087d34ba35686eed45ee2c992abf148
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-24 02:40:23 +0700

    Update Program Menghitung Keliling Segitiga.c

--- C/Program Menghitung Keliling Segitiga.c
+++ C/Program Menghitung Keliling Segitiga.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int sisi_1, sisi_2, sisi_3;
-    float keliling;
+    int sisi_1, sisi_2, sisi_3, sisi_4;
+    int keliling;
 
-    printf("Program Menghitung Keliling Segitiga\n");
+    printf("Program Menghitung Keliling Trapesium\n");
     printf("visit us hobingoding.com\n\n");
     // Input Sisi-1
     printf("Input Sisi-1 : ");
@@ -15,9 +15,12 @@ int main() {
     // Input Sisi-3
     printf("Input Sisi-3 : ");
     scanf("%d", &sisi_3);
+    // Input Sisi-4
+    printf("Input Sisi-4 : ");
+    scanf("%d", &sisi_4);
 
     // Hitung Keliling
-    keliling = sisi_1 + sisi_2 + sisi_3;
-    printf("Keliling Segitiga : %.2f", keliling);
+    keliling = sisi_1 + sisi_2 + sisi_3 + sisi_4;
+    printf("Keliling Trapesium : %d", keliling);
     return 0;
 }
