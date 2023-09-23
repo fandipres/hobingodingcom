98582a0510f4c582ee1ebc1bbf507cf720f111cf
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-23 18:52:40 +0700

    Update Program Mengubah Nilai Angka Menjadi Nilai Huruf.c

--- C/Program Mengubah Nilai Angka Menjadi Nilai Huruf.c
+++ C/Program Mengubah Nilai Angka Menjadi Nilai Huruf.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
-//visit us @hobingoding.com
 
-int main(){
+int main() {
     int nilai;
 
-    printf("Program Konversi Nilai Angka Menjadi Nilai Huruf\n\n");
-    printf("Input Nilai: ");
+    printf("Program Mengubah Nilai Angka Menjadi Nilai Huruf\n");
+    printf("visit us hobingoding.com\n\n");
+    // Input Nilai
+    printf("Input Nilai : ");
     scanf("%d", &nilai);
 
+    // Ubah Nilai
     if(nilai > 84 && nilai <= 100)
-        printf("Nilai Huruf: A");
+        printf("Nilai Huruf : A");
     else if(nilai > 79 && nilai <= 84)
-        printf("Nilai Huruf: A-");
+        printf("Nilai Huruf : A-");
     else if(nilai > 74 && nilai <= 79)
-        printf("Nilai Huruf: B+");
+        printf("Nilai Huruf : B+");
     else if(nilai > 69 && nilai <= 74)
-        printf("Nilai Huruf: B");
+        printf("Nilai Huruf : B");
     else if(nilai > 64 && nilai <= 69)
-        printf("Nilai Huruf: B-");
+        printf("Nilai Huruf : B-");
     else if(nilai > 59 && nilai <= 64)
-        printf("Nilai Huruf: C+");
+        printf("Nilai Huruf : C+");
     else if(nilai > 49 && nilai <= 59)
-        printf("Nilai Huruf: C");
+        printf("Nilai Huruf : C");
     else if(nilai > 39 && nilai <= 49)
         printf("Nilai Huruf: D");
     else if(nilai > 0 && nilai <= 39)
-        printf("Nilai Huruf: E");
+        printf("Nilai Huruf : E");
     else
-        printf("MAAF: Masukan Anda Salah");
-    printf("\n");
-
-    getch();
+        printf("MAAF : Masukan Anda Salah");
     return 0;
 }
