677c451f18f34267b45ca5b26091966f694d352c
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-24 02:48:02 +0700

    Update Program Menghitung Luas Trapesium.c

--- C/Program Menghitung Luas Trapesium.c
+++ C/Program Menghitung Luas Trapesium.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-//visit us @hobingoding.com
 
-int main(){
-    int sisi1, sisi2, tinggi;
-    int luas;
+int main() {
+    int sisi_1, sisi_2, tinggi, luas;
 
-    //Input Variabel
-    printf("Program Menghitung Luas Trapesium\n\n");
+    printf("Program Menghitung Luas Trapesium\n");
+    printf("visit us hobingoding.com\n\n");
+    // Input Sisi-1
     printf("Input Sisi-1 : ");
-    scanf("%d", &sisi1);
+    scanf("%d", &sisi_1);
+    // Input Sisi-2
     printf("Input Sisi-2 : ");
-    scanf("%d", &sisi2);
+    scanf("%d", &sisi_2);
+    // Input Tinggi
     printf("Input Tinggi : ");
     scanf("%d", &tinggi);
 
-    //Menghitung Luas
-    luas = (sisi1 + sisi2) * tinggi / 2;
-    printf("Luas         : %d cm^2\n", luas);
+    // Hitung Luas
+    luas = (sisi_1 + sisi_2) * tinggi / 2;
+    printf("Luas Trapesium : %d", luas);
     return 0;
 }
