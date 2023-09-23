24c0fd1a6aa95610ea842f956798067d31094695
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-24 01:17:56 +0700

    Update Program Mencetak Barisan Bilangan Ganjil Genap dalam Rentang Tertentu.c

--- C/Program Mencetak Barisan Bilangan Ganjil Genap dalam Rentang Tertentu.c
+++ C/Program Mencetak Barisan Bilangan Ganjil Genap dalam Rentang Tertentu.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-//visit us @hobingoding.com
 
-int main(){
-    int batasBawah;
-    int batasAtas;
+int main() {
+    int awal, akhir;
 
-    printf("Program Mencetak Barisan Bilangan Ganjil dan Genap dalam Rentang Tertentu\n\n");
-    printf("Batas Bawah : ");
-    scanf("%d", &batasBawah);
-    printf("Batas Atas : ");
-    scanf("%d", &batasAtas);
+    printf("Program Mencetak Barisan Bilangan Ganjil Genap dalam Rentang Tertentu\n");
+    printf("visit us hobingoding.com\n\n");
+    // Input Nilai Awal
+    printf("Input Nilai Awal : ");
+    scanf("%d", &awal);
+    // Input Nilai Akhir
+    printf("Input Nilai Akhir : ");
+    scanf("%d", &akhir);
 
+    // Barisan Bilangan Genap
     printf("Bilangan Genap : ");
-    for(int i = batasBawah; i <= batasAtas; i++){
+    for(int i = awal; i <= akhir; i++) {
         if(i % 2 == 0)
             printf("%d ", i);
     }
 
-    printf("\nBilangan Ganjil : ");
-    for(int i = batasBawah; i <= batasAtas; i++){
+    // Barisan Bilangan Ganjil
+    printf("\n");
+    printf("Bilangan Ganjil : ");
+    for(int i = awal; i <= akhir; i++) {
         if(i % 2 != 0)
             printf("%d ", i);
     }
-
-    getch();
     return 0;
 }
