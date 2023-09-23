cca70739a1192142d770f224dfab6155898b5c96
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-23 23:07:30 +0700

    Update Program Menghitung Jumlah Karakter.c

--- C/Program Menghitung Jumlah Karakter.c
+++ C/Program Menghitung Jumlah Karakter.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <string.h>
-//visit us @hobingoding.com
 
-int main(){
+int main() {
     char kalimat[100];
-    int jumlahKarakter;
+    int jumlah_karakter;
 
-    printf("Program Menghitung Jumlah Karakter\n\n");
-    printf("Input String : ");
+    printf("Program Menghitung Jumlah Karakter\n");
+    printf("visit us hobingoding.com\n\n");
+    // Input Kalimat
+    printf("Input Kalimat : ");
     gets(kalimat);
 
-    jumlahKarakter = strlen(kalimat);
-    printf("Jumlah Karakter : %d", jumlahKarakter);
-
-    getch();
+    // Hitung Jumlah Karakter
+    jumlah_karakter = strlen(kalimat);
+    printf("Jumlah Karakter : %d", jumlah_karakter);
     return 0;
 }
