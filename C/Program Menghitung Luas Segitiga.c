3b5236fc8015b7d253b3da8d229d993bf74d3824
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-23 14:02:47 +0700

    Update Program Menghitung Luas Segitiga.c

--- C/Program Menghitung Luas Segitiga.c
+++ C/Program Menghitung Luas Segitiga.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-//visit us @hobingoding.com
+// visit us @hobingoding.com
 
 int main(){
     int alas, tinggi;
     float luas;
 
     printf("Program Menghitung Luas Segitiga\n\n");
+    // Input Alas
     printf("Input Alas \t: ");
     scanf("%d", &alas);
+    // Input Tinggi
     printf("Input Tinggi \t: ");
     scanf("%d", &tinggi);
-
+    
+    // Hitung Luas
     luas = alas * tinggi * 0.5;
     printf("Luas Segitiga \t: %.2f cm^2\n", luas);
 
