673be02b9271f68de362f07699f86ffedd5b2b6d
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-23 23:12:08 +0700

    Update Program Menghitung Jumlah Huruf dan Angka.c

--- C/Program Menghitung Jumlah Huruf dan Angka.c
+++ C/Program Menghitung Jumlah Huruf dan Angka.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <ctype.h>
-//visit us @hobingoding.com
 
-int main(){
+int main() {
     char karakter;
-    int hurufBesar = 0, hurufKecil = 0;
-    int hurufVokal = 0, hurufKonsonan = 0;
+    int huruf_besar = 0, huruf_kecil = 0;
+    int huruf_vokal = 0, huruf_konsonan = 0;
     int spasi = 0, angka = 0;
 
-    printf("Program Menghitung Jumlah Karakter\n\n");
-    printf("Input String : ");
-    while((karakter = getchar())!='\n')
-    {
-        hurufKonsonan++;
+    printf("Program Menghitung Jumlah Huruf dan Angka\n");
+    printf("visit us hobingoding.com\n\n");
+    // Input Kalimat
+    printf("Input Kalimat : ");
+    while((karakter = getchar())!='\n') {
+        // Hitung Huruf dan Angka
+        huruf_konsonan++;
         if(karakter >= 'A' && karakter <= 'Z')
-            hurufBesar++;
+            huruf_besar++;
         if(karakter >= 'a' && karakter <= 'z')
-            hurufKecil++;
+            huruf_kecil++;
         if(toupper(karakter) == 'A' || toupper(karakter) == 'I' || toupper(karakter) == 'U' || toupper(karakter) == 'E' || toupper(karakter) == 'O')
-            hurufVokal++;
+            huruf_vokal++;
         if(karakter == ' ')
             spasi++;
         if(karakter >= '0' && karakter <= '9')
             angka++;
     }
-    hurufKonsonan -= (spasi + angka + hurufVokal);
+    huruf_konsonan -= spasi + angka + huruf_vokal;
 
     printf("Hasil Penghitungan :\n");
-    printf("Kapital     : %d\n", hurufBesar);
-    printf("Non-Kapital : %d\n", hurufKecil);
-    printf("Vokal       : %d\n", hurufVokal);
-    printf("Konsonan    : %d\n", hurufKonsonan);
-    printf("Angka       : %d\n", angka);
-    printf("Spasi       : %d\n", spasi);
-    getch();
+    printf("Kapital : %d\n", huruf_besar);
+    printf("Non-Kapital : %d\n", huruf_kecil);
+    printf("Vokal : %d\n", huruf_vokal);
+    printf("Konsonan : %d\n", huruf_konsonan);
+    printf("Angka : %d\n", angka);
+    printf("Spasi : %d\n", spasi);
     return 0;
 }
