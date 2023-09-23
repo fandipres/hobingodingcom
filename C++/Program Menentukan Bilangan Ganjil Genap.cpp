a5588c48df291e513630c1d50864f20843ac405c
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-23 15:41:32 +0700

    Update Program Menentukan Bilangan Ganjil Genap.cpp

--- C++/Program Menentukan Bilangan Ganjil Genap.cpp
+++ C++/Program Menentukan Bilangan Ganjil Genap.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 using namespace std;
-//source code by hobingoding.com
 
 int main(){
     int bilangan;
     
     cout << "Program Menentukan Bilangan Ganjil Genap" << endl;
-    cout << "source code by hobingoding.com\n" << endl;
-    
-    //Penginputan Bilangan yang akan Dicek
+    cout << "visit us hobingoding.com\n" << endl;
+    // Input Bilangan
     cout << "Input Bilangan : ";
     cin >> bilangan;
     
-    //Penghitungan dan Pengecekan Hasil Modulo
+    // Cek Ganjil Genap
     if(bilangan % 2 == 0)
         cout << "Hasil : Bilangan Genap" << endl;
     else
         cout << "Hasil : Bilangan Ganjil" << endl;
-
     cin.ignore();
     return 0;
 }
