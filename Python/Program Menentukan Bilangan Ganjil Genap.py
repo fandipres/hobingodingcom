a42bf709a05de43361434eebebf0195982e4be6c
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-23 17:01:40 +0700

    Update Program Menentukan Bilangan Ganjil Genap.py

--- Python/Program Menentukan Bilangan Ganjil Genap.py
+++ Python/Program Menentukan Bilangan Ganjil Genap.py
@@ -1,10 +1,11 @@
-print("Program Mengecek Angka Ganjil atau Genap")
-print("visit us @hobingoding.com")
+print("Program Menentukan Bilangan Ganjil Genap")
+print("visit us hobingoding.com")
 print()
-#Input Angka
-angka = int(input("Input Angka : "))
-#Pengecekan Angka
-if(angka % 2 == 0):
-    print("Angka", angka, "merupakan angka Genap.")
+# Input Bilangan
+bilangan = int(input("Input Bilangan : "))
+
+# Cek Ganjil Genap
+if(bilangan % 2 == 0):
+    print("Hasil : Bilangan Genap")
 else:
-    print("Angka", angka, "merupakan angka Ganjil.")
+    print("Hasil : Bilangan Ganjil")
