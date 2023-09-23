c1241348109756a521c4373a5d211e939ba8aa55
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-24 02:46:09 +0700

    Update Program Menghitung Luas Layang-layang.c

--- C/Program Menghitung Luas Layang-layang.c
+++ C/Program Menghitung Luas Layang-layang.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-//visit us @hobingoding.com
 
-int main(){
-    int diagonal1, diagonal2;
+int main() {
+    int diagonal_1, diagonal_2;
     int luas;
 
-    //Input Variabel
-    printf("Program Menghitung Luas Layang-layang\n\n");
+    printf("Program Menghitung Luas Layang-layang\n");
+    printf("visit us hobingoding.com\n\n");
+    // Input Diagonal-1
     printf("Input Diagonal-1 : ");
-    scanf("%d", &diagonal1);
+    scanf("%d", &diagonal_1);
+    // Input Diagonal-2
     printf("Input Diagonal-2 : ");
-    scanf("%d", &diagonal2);
+    scanf("%d", &diagonal_2);
 
-    //Menghitung Luas
-    luas = (diagonal1 * diagonal2) / 2;
-    printf("Luas             : %d cm^2\n", luas);
+    // Hitung Luas
+    luas = (diagonal_1 * diagonal_2) / 2;
+    printf("Luas Layang-layang : %d", luas);
     return 0;
 }
