d79d235f6a142e7a755e429134c6268c74c3e00f
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-24 19:53:21 +0700

    Update Program Menghitung Harga Jual Barang.py

--- Python/Program Menghitung Harga Jual Barang.py
+++ Python/Program Menghitung Harga Jual Barang.py
@@ -1,12 +1,17 @@
 print("Program Menghitung Harga Jual Barang")
-print("visit us @hobingoding.com")
+print("visit us hobingoding.com")
 print()
+# Input Harga Beli
 harga_beli = int(input("Input Harga Beli : "))
+# Input Opsi Perhitungan
 print("Opsi Perhitungan : ")
 print("1. Untung")
 print("2. Rugi")
 opsi = int(input("Input Opsi Perhitungan : "))
+# Input Persentase
 persen = int(input("Input Persentase Untung-Rugi : "))
+
+# Hitung Harga Jual
 if(opsi == 1):
     harga_jual = harga_beli+(harga_beli*persen/100)
     print("Harga Jual jika untung sebesar %s persen adalah %s"%(persen, harga_jual))
