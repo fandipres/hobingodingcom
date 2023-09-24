c24c8a7a35696025375861a70d739360e4809990
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-24 14:01:43 +0700

    Update Program Bubble Sort.cpp

--- C++/Program Bubble Sort.cpp
+++ C++/Program Bubble Sort.cpp
@@ -13,18 +13,14 @@ int main(){
     
     // Input Data
     cout << "Input Data : ";
-    for(i = 0; i < banyak_data; i++)
-    {
+    for(i = 0; i < banyak_data; i++) {
         cin >> data[i];
     }
     
     // Algoritma Bubble Sort
-    for(i = 0; i < banyak_data; i++)
-    {
-        for(j = 0; j < banyak_data-i-1; j++)
-        {
-            if(data[j] > data[j+1])
-            {
+    for(i = 0; i < banyak_data; i++) {
+        for(j = 0; j < banyak_data-i-1; j++) {
+            if(data[j] > data[j+1]) {
                 int tmp = data[j];
                 data[j] = data[j+1];
                 data[j+1] = tmp;
@@ -34,8 +30,7 @@ int main(){
     
     // Hasil Pengurutan Data
     cout << "Hasil Pengurutan Data : ";
-    for(i = 0; i < banyak_data; i++)
-    {
+    for(i = 0; i < banyak_data; i++) {
         cout << data[i] << " ";
     }
     cin.ignore();
