0ec19bc0db5ca5edea8a01c559080118096a9ef4
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-24 19:28:40 +0700

    Update Program Insertion Sort.py

--- Python/Program Insertion Sort.py
+++ Python/Program Insertion Sort.py
@@ -1,20 +1,23 @@
 print("Pengurutan Data dengan Algoritma Insertion Sort")
-print("visit us @hobingoding.com")
+print("visit us hobingoding.com")
 print()
-#Input Banyak Data
-banyakData = int(input("Input Banyak Data : "))
+# Input Banyak Data
+banyak_data = int(input("Input Banyak Data : "))
 data = []
-#Input Data
-print("Data yang Belum Terurut :")
-for i in range(banyakData):
+
+# Input Data
+print("Input Data :")
+for i in range(banyak_data):
     data.append(int(input()))
-#Algoritma Insertion Sort
-for i in range(banyakData):
+
+# Algoritma Insertion Sort
+for i in range(banyak_data):
     temp = data[i]
     j = i - 1
     while(j >= 0 and temp < data[j]):
         data[j+1] = data[j]
         j -= 1
     data[j + 1] = temp
-#Output Data
+
+# Hasil Pengurutan Data
 print("Hasil Pengurutan Data :", data)
