6831d130d6f3e3e67ad976ed905c1ddb692941fd
Fandi Presly Simamora <49110452+fandipres@users.noreply.github.com>
2023-09-24 19:11:32 +0700

    Update Program Menghitung Keliling dan Luas Persegi.py

--- Python/Program Menghitung Keliling dan Luas Persegi.py
+++ Python/Program Menghitung Keliling dan Luas Persegi.py
@@ -1,8 +1,12 @@
 print("Program Menghitung Keliling dan Luas Persegi")
-print("visit us @hobingoding.com")
+print("visit us hobingoding.com")
 print()
-#Input Sisi
+# Input Sisi
 sisi = int(input("Input Sisi : "))
-#Hitung Keliling dan Luas
-print("Keliling : ", 4 * sisi)
-print("Luas : ", sisi * sisi)
+
+# Hitung Keliling
+keliling = 4 * sisi
+# Hitung Luas
+luas = sisi * sisi
+print("Keliling Persegi :", keliling)
+print("Luas Persegi :", luas)
