fc87b5b6655c67b7544d40d14be1bd3cf2cfb41e
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 22:53:21 +0700

    Add SC Program Menghitung Keliling Lingkaran

---
+++ Python/Program Menghitung Keliling Lingkaran.py
@@ -0,0 +1,9 @@
+print("Program Menghitung Keliling Lingkaran")
+print("visit us hobingoding.com")
+print()
+# Input Jari-jari
+r = int(input("Input Jari-jari : "))
+
+# Hitung Keliling
+keliling = 3.14 * 2 * r
+print("Keliling Lingkaran : %.2f"%(keliling))
