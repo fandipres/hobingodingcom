bd69fc520fdc06dcbd82b7dd3d3a09c95e000d38
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 23:45:46 +0700

    Add SC Program Menghitung Luas Jajar Genjang

---
+++ Python/Program Menghitung Luas Jajar Genjang.py
@@ -0,0 +1,11 @@
+print("Program Menghitung Luas Jajar Genjang")
+print("visit us hobingoding.com")
+print()
+# Input Alas
+alas = int(input("Input Alas : "))
+# Input Tinggi
+tinggi = int(input("Input Tinggi : "))
+
+# Hitung Luas
+luas = alas * tinggi
+print("Luas Jajar Genjang :", luas)
