5fc9e2abab99e4a9f7b5da0bb966232de25073e9
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 22:46:33 +0700

    Add SC Program Menghitung Luas Segi Enam

---
+++ Python/Program Menghitung Luas Segi Enam.py
@@ -0,0 +1,12 @@
+print("Program Menghitung Luas Segi Enam")
+print("visit us hobingoding.com")
+print()
+# Import Library
+import math
+
+# Input Sisi
+sisi = int(input("Input Sisi : "))
+
+# Hitung Luas
+luas = (3 / 2 * math.sqrt(3)) * sisi * sisi
+print("Luas Segi Enam : %.2f"%(luas))
