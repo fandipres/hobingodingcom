b5475d7cbc9ed40f5c14a925bbcd8c6f0e6768a6
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 22:36:37 +0700

    Add SC Program Menghitung Volume Bola

---
+++ Python/Program Menghitung Volume Bola.py
@@ -0,0 +1,10 @@
+print("Program Menghitung Volume Bola")
+print("visit us hobingoding.com")
+print()
+phi = 3.14
+# Input Jari-jari
+r = int(input("Input Jari-jari : "))
+
+# Hitung Volume
+volume = (4 / 3) * phi * r * r * r
+print("Volume Bola : %.2f"%volume)
