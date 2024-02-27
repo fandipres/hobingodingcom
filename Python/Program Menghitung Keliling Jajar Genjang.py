042d35cd865a99067989df82b06a27b5795de03a
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 13:59:35 +0700

    Add SC Program Menghitung Keliling Jajar Genjang

---
+++ Python/Program Menghitung Keliling Jajar Genjang.py
@@ -0,0 +1,11 @@
+print("Program Menghitung Keliling Jajargenjang")
+print("visit us hobingoding.com")
+print()
+# Input Alas
+alas = int(input("Input Alas : "))
+# Input Sisi
+sisi_miring = int(input("Input Sisi Miring : "))
+
+# Hitung Keliling
+keliling = 2 * (alas + sisi_miring)
+print("Keliling Jajar Genjang :", keliling)
