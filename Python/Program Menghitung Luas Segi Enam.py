25fe76394ca57af0f472abc58124351e7cde2957
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 22:11:23 +0700

    Add SC Program Menghitung Luas Segi Enam

---
+++ Python/Program Menghitung Luas Segi Enam.py
@@ -0,0 +1,9 @@
+print("Program Menghitung Keliling Segi Enam")
+print("visit us hobingoding.com")
+print()
+# Input Sisi
+sisi = int(input("Input Sisi : "))
+
+# Hitung Keliling
+keliling = 6 * sisi
+print("Keliling Segi Enam :", keliling)
