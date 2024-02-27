e704159b74416845a5c9bd1abad51e8434a45bd0
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 23:52:15 +0700

    Add SC Program Menghitung Luas Layang-layang

---
+++ Python/Program Menghitung Luas Layang-layang.py
@@ -0,0 +1,11 @@
+print("Program Menghitung Luas Layang-layang")
+print("visit us hobingoding.com")
+print()
+# Input Diagonal-1
+diagonal_1 = int(input("Input Diagonal-1 : "))
+# Input Diagonal-2
+diagonal_2 = int(input("Input Diagonal-2 : "))
+
+# Hitung Luas
+luas = diagonal_1 * diagonal_2 / 2
+print("Luas Layang-layang :", luas)
