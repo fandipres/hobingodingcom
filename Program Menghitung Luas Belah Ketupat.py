ce09e25533cd834d12d85e3ac0685ced06108102
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 23:34:50 +0700

    Add SC Program Menghitung Luas Belah Ketupat

---
+++ Program Menghitung Luas Belah Ketupat.py
@@ -0,0 +1,11 @@
+print("Program Menghitung Luas Belah Ketupat")
+print("visit us hobingoding.com")
+print()
+# Input Diagonal-1
+diagonal_1 = int(input("Input Diagonal-1 : "))
+# Input Diagonal-2
+diagonal_2 = int(input("Input Diagonal-2 : "))
+
+# Hitung Luas
+luas = 0.5 * diagonal_1 * diagonal_2
+print("Luas Belah Ketupat :", luas)
