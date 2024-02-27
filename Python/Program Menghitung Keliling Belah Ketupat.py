e7477e657a355bc3c730b40c2ec23870e82c9645
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 13:13:29 +0700

    Add SC Program Menghitung Keliling Belah Ketupat

---
+++ Python/Program Menghitung Keliling Belah Ketupat.py
@@ -0,0 +1,9 @@
+print("Program Menghitung Keliling Belah Ketupat")
+print("visit us hobingoding.com")
+print()
+# Input Sisi
+sisi = int(input("Input Sisi : "))
+
+# Hitung Keliling
+keliling = 4 * sisi
+print("Keliling Belah Ketupat :", keliling)
