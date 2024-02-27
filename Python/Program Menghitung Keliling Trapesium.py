fbb1a334865d7501771ffca8b99baa09bf71c96a
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 23:25:42 +0700

    Add SC Program Menghitung Keliling Trapesium

---
+++ Python/Program Menghitung Keliling Trapesium.py
@@ -0,0 +1,15 @@
+print("Program Menghitung Keliling Trapesium")
+print("visit us hobingoding.com")
+print()
+# Input Sisi-1
+sisi_1 = int(input("Input Sisi-1 : "))
+# Input Sisi-2
+sisi_2 = int(input("Input Sisi-2 : "))
+# Input Sisi-3
+sisi_3 = int(input("Input Sisi-3 : "))
+# Input Sisi-4
+sisi_4 = int(input("Input Sisi-4 : "))
+
+# Hitung Keliling
+keliling = sisi_1 + sisi_2 + sisi_3 + sisi_4
+print("Keliling Trapesium :", keliling)
