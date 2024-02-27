e78dbcf608281e2dc84c8c4db710e336d0bd7042
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-27 21:54:26 +0700

    Add SC Program Menghitung Keliling Segitiga

---
+++ Python/Program Menghitung Keliling Segitiga.py
@@ -0,0 +1,13 @@
+print("Program Menghitung Keliling Segitiga")
+print("visit us hobingoding.com")
+print()
+# Input Sisi-1
+sisi_1 = int(input("Input Sisi-1 : "))
+# Input Sisi-2
+sisi_2 = int(input("Input Sisi-2 : "))
+# Input Sisi-3
+sisi_3 = int(input("Input Sisi-3 : "))
+
+# Hitung Keliling
+keliling = sisi_1 + sisi_2 + sisi_3
+print("Keliling Segitiga :", keliling)
