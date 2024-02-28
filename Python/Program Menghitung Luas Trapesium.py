b6e57b4ed4de8d0e3337b5d30edeb213d78b4f99
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-28 22:23:16 +0700

    Add SC Program Menghitung Luas Trapesium

---
+++ Python/Program Menghitung Luas Trapesium.py
@@ -0,0 +1,13 @@
+print("Program Menghitung Luas Trapesium")
+print("visit us hobingoding.com")
+print()
+# Input Sisi-1
+sisi_1 = int(input("Input Sisi-1 : "))
+# Input Sisi-2
+sisi_2 = int(input("Input Sisi-2 : "))
+# Input Tinggi
+tinggi = int(input("Input Tinggi : "))
+
+# Hitung Luas
+luas = (sisi_1 + sisi_2) * tinggi / 2
+print("Luas Trapesium :", luas)
