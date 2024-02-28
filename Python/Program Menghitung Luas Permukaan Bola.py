4d6019a499dd2aa7bab94f5346400a811f5499e0
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-02-28 22:14:54 +0700

    Add SC Program Menghitung Luas Permukaan Bola

---
+++ Python/Program Menghitung Luas Permukaan Bola.py
@@ -0,0 +1,10 @@
+print("Program Menghitung Luas Permukaan Bola")
+print("visit us hobingoding.com")
+print()
+phi = 3.14
+# Input Jari-jari
+r = int(input("Input Jari-jari : "))
+
+# Hitung Luas Permukaan
+luas_permukaan = 4 * phi * r * r
+print("Luas Permukaan Bola : %.2f"%luas_permukaan)
