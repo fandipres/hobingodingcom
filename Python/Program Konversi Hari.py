c6268d9e0f33347c040c3ff5d0352356bcb5c996
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-04-05 00:35:15 +0700

    Add SC Program Konversi Hari

---
+++ Python/Program Konversi Hari.py
@@ -0,0 +1,16 @@
+print("Program Konversi Hari")
+print("visit us hobingoding.com")
+print()
+# Input hari
+hari = int(input("INPUT HARI : "))
+
+# Konversi Hari
+tahun = hari // 365
+hari = hari % 365
+
+bulan = hari // 30
+hari %= 30
+
+minggu = hari // 7
+hari %= 7
+print("HASIL : %d TAHUN %d BULAN %d MINGGU %d HARI"%(tahun, bulan, minggu, hari))
