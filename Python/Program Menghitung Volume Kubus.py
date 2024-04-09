a83c7cefd77044ff1608782c27c783824e3926b1
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-04-10 01:30:43 +0700

    Add SC Program Menghitung Volume Kubus

---
+++ Python/Program Menghitung Volume Kubus.py
@@ -0,0 +1,9 @@
+print("Program Menghitung Volume Kubus")
+print("visit us hobingoding.com")
+print()
+# Input Sisi
+sisi = int(input("INPUT SISI : "))
+
+# Hitung Voume
+volume = sisi**3
+print("VOLUME KUBUS :", volume)
