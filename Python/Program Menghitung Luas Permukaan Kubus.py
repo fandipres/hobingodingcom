2a9c0c326437cdb8800a68decf664b4c4693414a
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-04-10 01:27:26 +0700

    Add SC Program Menghitung Luas Permukaan Kubus

---
+++ Python/Program Menghitung Luas Permukaan Kubus.py
@@ -0,0 +1,9 @@
+print("Program Menghitung Luas Permukaan Kubus")
+print("visit us hobingoding.com")
+print()
+# Input Sisi
+sisi = int(input("INPUT SISI : "))
+
+# Hitung Luas Permukaan
+luas_permukaan = 6 * sisi**2
+print("LUAS PERMUKAAN KUBUS :", luas_permukaan)
