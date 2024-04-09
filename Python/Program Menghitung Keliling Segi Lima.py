2aac3d7e21745a39bb31c52c439eb2d8712c30b5
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-04-09 22:14:24 +0700

    Add SC Program Menghitung Keliling Segi Lima

---
+++ Python/Program Menghitung Keliling Segi Lima.py
@@ -0,0 +1,9 @@
+print("Program Menghitung Keliling Segi Lima")
+print("visit us hobingoding.com")
+print()
+# Input Sisi
+sisi = int(input("INPUT SISI : "))
+
+# Hitung Keliling
+keliling = 5 * sisi
+print("KELILING SEGI LIMA :", keliling)
