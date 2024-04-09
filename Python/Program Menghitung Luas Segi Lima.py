21e5fa652ce94466432c9ecd43176e7930c28e64
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-04-09 20:25:19 +0700

    Add SC Program Menghitung Luas Segi Lima

---
+++ Python/Program Menghitung Luas Segi Lima.py
@@ -0,0 +1,9 @@
+print("Program Menghitung Luas Segi Lima")
+print("visit us hobingoding.com")
+print()
+# Input Sisi
+sisi = int(input("INPUT SISI : "))
+
+# Hitung Luas
+luas = 1 / 4 * (5 * (5 + 2 * 5**0.5))**0.5 * sisi**2
+print("LUAS SEGI LIMA : %.2f"%(luas))
