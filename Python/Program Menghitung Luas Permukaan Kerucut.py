1a1fa1c5ccf9a40dac618cdfbbcad8e02b02acbe
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-04-10 17:54:25 +0700

    Add SC Program Menghitung Luas Permukaan Kerucut

---
+++ Python/Program Menghitung Luas Permukaan Kerucut.py
@@ -0,0 +1,11 @@
+print("Program Menghitung Luas Permukaan Kerucut")
+print("visit us hobingoding.com")
+print()
+# Input Jari-jari
+r = int(input("INPUT JARI-JARI : "))
+# Input Sisi
+sisi = int(input("INPUT SISI : "))
+
+# Hitung Luas Permukaan
+luas_permukaan = 3.14 * r * (r + sisi)
+print("LUAS PERMUKAAN KERUCUT : %.2f"%luas_permukaan)
