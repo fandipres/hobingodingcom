e3d389be062fe9d93c6f31a091d283756732adac
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-04-10 10:53:58 +0700

    Add SC Program Menghitung Luas Permukaan Tabung

---
+++ Python/Program Menghitung Luas Permukaan Tabung.py
@@ -0,0 +1,11 @@
+print("Program Menghitung Luas Permukaan Tabung")
+print("visit us hobingoding.com")
+print()
+# Input Jari-jari
+r = int(input("INPUT JARI-JARI : "))
+# Input Tinggi
+t = int(input("INPUT TINGGI : "))
+
+# Hitung Luas Permukaan
+luas_permukaan = 2 * 3.14 * r * (r + t)
+print("LUAS PERMUKAAN TABUNG : %.2f"%luas_permukaan)
