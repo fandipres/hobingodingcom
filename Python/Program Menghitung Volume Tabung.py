663438417b54b25687c337bf9b2429396545e952
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-04-10 10:49:56 +0700

    Add SC Program Menghitung Volume Tabung

---
+++ Python/Program Menghitung Volume Tabung.py
@@ -0,0 +1,11 @@
+print("Program Menghitung Volume Tabung")
+print("visit us hobingoding.com")
+print()
+# Input Jari-jari
+r = int(input("INPUT JARI-JARI : "))
+# Input Tinggi
+t = int(input("INPUT TINGGI : "))
+
+# Hitung Voume
+volume = 3.14 * r**2 * t
+print("VOLUME TABUNG : %.2f"%volume)
