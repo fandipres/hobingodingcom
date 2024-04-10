ed58509076db0d003a0426b2b59c405411e22c8a
Fandi Presly Simamora <fandi.simamora@gmail.com>
2024-04-10 17:54:43 +0700

    Add SC Program Menghitung Volume Kerucut

---
+++ Python/Program Menghitung Volume Kerucut.py
@@ -0,0 +1,11 @@
+print("Program Menghitung Volume Kerucut")
+print("visit us hobingoding.com")
+print()
+# Input Jari-jari
+r = int(input("INPUT JARI-JARI : "))
+# Input Tinggi
+t = int(input("INPUT TINGGI : "))
+
+# Hitung Volume
+volume = 1 / 3 * 3.14 * r**2 * t
+print("VOLUME KERUCUT : %.2f"%volume)
